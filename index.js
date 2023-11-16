2780a43c411a2c6f4982e6604b7658e0bd8bbb57
S H-SuHan <suhanur35-448@diu.edu.bd>
2023-11-16 14:55:46 +0600

    User Get And Post here

--- index.js
+++ index.js
@@ -32,6 +32,8 @@ async function run() {
         const menuCollection = client.db("menuDB").collection("menu")
         const reviewCollection = client.db("menuDB").collection("review")
         const cartCollection = client.db("menuDB").collection("cart")
+        const userCollection = client.db("menuDB").collection("user")
+
 
 
 
@@ -50,6 +52,22 @@ async function run() {
             const email = req.query.email
             const query = {email: email}
             const result = await cartCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.get("/user", async (req, res) =>{
+            const result = await userCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.post("/user", async (req, res)=>{
+            const user = req.body
+            const query = {email : user.email}
+            const existingUser = await userCollection.findOne(query)
+            if (existingUser) {
+                return res.send({massage : "user is Already Exist"})
+            }
+            const result = await userCollection.insertOne(user)
             res.send(result)
         })
 
