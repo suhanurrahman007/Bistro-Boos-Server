88cb256a863b6238292180bd825d2b9e7a29cbdf
S H-SuHan <suhanur35-448@diu.edu.bd>
2023-11-16 15:09:29 +0600

    User Get And Post here

--- index.js
+++ index.js
@@ -71,6 +71,13 @@ async function run() {
             res.send(result)
         })
 
+        app.delete("/user/:id", async(req, res)=>{
+            const id = req.params.id
+            const query = {_id : new ObjectId(id)}
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
+        })
+
         app.post("/cart", async (req, res) =>{
             const cart = req.body
             const result = await cartCollection.insertOne(cart)
