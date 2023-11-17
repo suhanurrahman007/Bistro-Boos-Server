d1f3cdf0ae8a80ad4dec70132275de108a172fc7
S H-SuHan <suhanur35-448@diu.edu.bd>
2023-11-18 02:21:24 +0600

    user and admin verify here

--- index.js
+++ index.js
@@ -37,21 +37,27 @@ async function run() {
         const userCollection = client.db("menuDB").collection("user")
 
 
-        app.post("/jwt", async(req, res) =>{
+        app.post("/jwt", async (req, res) => {
             const user = req.body
-            const token = jwt.sign(user, process.env.DB_BISTRO_BOSS_TOKEN, {expiresIn: '1h'})
-            res.send({token})
+            const token = jwt.sign(user, process.env.DB_BISTRO_BOSS_TOKEN, {
+                expiresIn: '1h'
+            })
+            res.send({
+                token
+            })
         })
 
 
-        const verifyToken = (req, res, next)=>{
+        const verifyToken = (req, res, next) => {
             console.log("inside Verify token", req.headers.authorization);
             if (!req.headers.authorization) {
-                return res.status(401).send({message : "forbidden access"})
+                return res.status(401).send({
+                    message: "forbidden access"
+                })
             }
             const token = req.headers.authorization.split(" ")[1]
 
-            jwt.verify(token, process.env.DB_BISTRO_BOSS_TOKEN, (err, decoded) =>{
+            jwt.verify(token, process.env.DB_BISTRO_BOSS_TOKEN, (err, decoded) => {
                 if (err) {
                     return res.status(401).send({
                         message: "forbidden access"
@@ -62,8 +68,19 @@ async function run() {
             })
         }
 
+        const verifyAdmin = async (req, res, next)=>{
+            const email = req.decoded.email
+            const query = {email: email}
+            const user = await userCollection.findOne(query)
+            const isAdmin = user?.role === 'admin'
+            if (!isAdmin) {
+                return res.status(403).send({massage: "forbidden access"})
+            }
+            next()
+        }
 
-        app.get("/menu", async(req, res) =>{
+
+        app.get("/menu", async (req, res) => {
             const result = await menuCollection.find().toArray()
             res.send(result)
         })
@@ -73,35 +90,63 @@ async function run() {
             res.send(result)
         })
 
-        app.get("/cart", async(req, res) =>{
+        app.get("/cart", async (req, res) => {
             const email = req.query.email
-            const query = {email: email}
+            const query = {
+                email: email
+            }
             const result = await cartCollection.find(query).toArray()
             res.send(result)
         })
 
-        app.get("/user",verifyToken, async (req, res) =>{
+        app.get("/user", verifyToken, verifyAdmin, async (req, res) => {
             const result = await userCollection.find().toArray()
             res.send(result)
         })
 
-        app.post("/user", async (req, res)=>{
+        app.get("/user/admin/:email", verifyToken, async (req, res) => {
+            const email = req.params.email
+            if (email !== req.decoded.email) {
+                return res.status(403).send({
+                    massage: "unAuthorize"
+                })
+            }
+            const query = {
+                email: email
+            }
+            const user = await userCollection.findOne(query)
+            let admin = false;
+            if (user) {
+                admin = user?.role === "admin"
+            }
+            res.send({
+                admin
+            })
+        })
+
+        app.post("/user", async (req, res) => {
             const user = req.body
-            const query = {email : user.email}
+            const query = {
+                email: user.email
+            }
             const existingUser = await userCollection.findOne(query)
             if (existingUser) {
-                return res.send({massage : "user is Already Exist"})
+                return res.send({
+                    massage: "user is Already Exist"
+                })
             }
             const result = await userCollection.insertOne(user)
             res.send(result)
         })
 
-        app.patch("/user/admin/:id", async(req, res) =>{
+        app.patch("/user/admin/:id", async (req, res) => {
             const id = req.params.id
-            const query = {_id : new ObjectId(id)}
+            const query = {
+                _id: new ObjectId(id)
+            }
             const updatedDoc = {
                 $set: {
-                    role : 'admin'
+                    role: 'admin'
                 }
             }
 
@@ -109,22 +154,26 @@ async function run() {
             res.send(result)
         })
 
-        app.delete("/user/:id", async(req, res)=>{
+        app.delete("/user/:id", async (req, res) => {
             const id = req.params.id
-            const query = {_id : new ObjectId(id)}
+            const query = {
+                _id: new ObjectId(id)
+            }
             const result = await userCollection.deleteOne(query)
             res.send(result)
         })
 
-        app.post("/cart", async (req, res) =>{
+        app.post("/cart", async (req, res) => {
             const cart = req.body
             const result = await cartCollection.insertOne(cart)
             res.send(result)
         })
 
-        app.delete("/cart/:id", async(req, res) =>{
+        app.delete("/cart/:id", async (req, res) => {
             const id = req.params.id
-            const query = {_id : new ObjectId(id)}
+            const query = {
+                _id: new ObjectId(id)
+            }
             const result = await cartCollection.deleteOne(query)
             res.send(result)
         })
@@ -144,9 +193,9 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.send("Bistro Server is Running")
 })
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Bistro Boss Server Running Port is ${port}`);
 })
